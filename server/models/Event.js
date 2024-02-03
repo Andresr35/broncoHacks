c886938cd42037e5f02561f5ec54bbd2f3cc0e65
random-debug <85326677+p-vinh@users.noreply.github.com>
2024-02-03 11:01:17 -0800

    Update Event.js

--- server/models/Event.js
+++ server/models/Event.js
@@ -7,6 +7,8 @@ const EventSchema = new Schema({
     picture: { type: String, required: false },
     posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
     events: [{ type: Schema.Types.ObjectId, ref: "Event" }],
+    type : { type: String, required: true, trim: true, enum: ["Study", "School", "Club", "ASI", "Personal"]},
+    attendees: [{ type: Schema.Types.ObjectId, ref: "User" }],
     meeting_time: { type: String, required: true, trim: true },
     meeting_location: { type: String, required: true, trim: true }
 });
