c4bbca73eedb123fca6ceaa004a2b557963a51dd
Andresr35 <andresjruiz@cpp.edu>
2024-02-03 13:05:35 -0800

    fix url

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ async function main() {
 
 var app = express();
 
-app.use(cors({ origin: "https://bronco-hacks.vercel.app/" }));
+app.use(cors({ origin: "https://bronco-hacks.vercel.app" }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
