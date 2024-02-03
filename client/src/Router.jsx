c63acb16dc36a35e185f29da35876af76a8622a1
Andresr35 <andresjruiz@cpp.edu>
2024-02-02 18:38:48 -0800

    set up router and pages

---
+++ client/src/Router.jsx
@@ -0,0 +1,28 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import ClubProfile from "./pages/ClubProfile";
+import SchoolPage from "./pages/SchoolPage";
+import ClassProfile from "./pages/ClassProfile";
+import UserProfile from "./pages/UserProfile";
+
+const Router = () => {
+  const url =
+    // eslint-disable-next-line no-undef
+    process.env.NODE_ENV == "development"
+      ? "http://localhost:3000"
+      : "https://serenebook-production.up.railway.app";
+
+  const router = createBrowserRouter([
+    { path: "/", element: <Home url={url} /> },
+    { path: "/login", element: <Login url={url} /> },
+    { path: "/profile/:profileID", element: <UserProfile url={url} /> },
+    { path: "/profile/:profileID", element: <ClassProfile url={url} /> },
+    { path: "/profile/:profileID", element: <SchoolPage url={url} /> },
+    { path: "/profile/:profileID", element: <ClubProfile url={url} /> },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
---
+++ client/src/pages/UserProfile.jsx
@@ -0,0 +1,9 @@
+import PropTypes from "prop-types";
+
+const UserProfile = (props) => {
+  return <div>UserProfile</div>;
+};
+
+UserProfile.propTypes = {};
+
+export default UserProfile;
