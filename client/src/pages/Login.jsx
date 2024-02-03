02dc0255035ad7fc14775f8d1a4b3b0f278e8c10
Andresr35 <andresjruiz@cpp.edu>
2024-02-03 02:38:35 -0800

    sign up css

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -6,7 +6,11 @@ const Login = (props) => {
   const [login, setLogin] = useState(true);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [signUpData, setSignUpData] = useState({ email: "" });
 
+  const authenticate = (e) => {
+    e.preventDefault();
+  };
   return (
     <main>
       <h1 className={styles.title}>Bronco Life</h1>
@@ -23,30 +27,94 @@ const Login = (props) => {
             login ? styles.signIn : styles.active + " " + styles.signIn
           }
         >
-          <h3>Sign In</h3>
+          <h3>Sign Up</h3>
         </div>
       </div>
-      <div className={styles.logInContainer}>
-        <h3>Log In</h3>
-        <div className={styles.input}>
-          <h4>Email</h4>
-          <input
-            type="text"
-            placeholder="Enter Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+      {login && (
+        <div className={styles.loginContainer}>
+          <h3>Log In</h3>
+          <form onSubmit={(e) => authenticate(e)}>
+            <div className={styles.input}>
+              <h4>Email</h4>
+              <input
+                type="text"
+                placeholder="Enter Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className={styles.input}>
+              <h4>Password</h4>
+              <input
+                type="text"
+                placeholder="Enter Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button className={styles.button} type="submit">
+              Log In
+            </button>
+          </form>
         </div>
-        <div className={styles.input}>
-          <h4>Password</h4>
-          <input
-            type="text"
-            placeholder="Enter Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+      )}
+      {!login && (
+        <div className={styles.signInContainer}>
+          <h3>Sign Up</h3>
+          <form>
+            <div className={styles.input}>
+              <h4>Email</h4>
+              <input
+                type="text"
+                placeholder="Enter Email"
+                value={signUpData.email}
+                onChange={(e) =>
+                  setSignUpData({ ...signUpData, email: e.target.value })
+                }
+              />
+            </div>
+            <div className={styles.input}>
+              <h4>Password</h4>
+              <input
+                type="text"
+                placeholder="Enter Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className={styles.input}>
+              <h4>Confirm Password</h4>
+              <input
+                type="text"
+                placeholder="Enter Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className={styles.input}>
+              <h4>Password</h4>
+              <input
+                type="text"
+                placeholder="Enter Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className={styles.input}>
+              <h4>Password</h4>
+              <input
+                type="text"
+                placeholder="Enter Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button className={styles.button} type="submit">
+              Sign Up
+            </button>
+          </form>
         </div>
-      </div>
+      )}
     </main>
   );
 };
