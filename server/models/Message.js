c24874f7697c798ae66217d920a62699c0e78a1d
random-debug <85326677+p-vinh@users.noreply.github.com>
2024-02-02 17:45:33 -0800

    Revert

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,42 +1,11 @@
-const {
-  DynamoDBClient,
-  CreateTableCommand,
-} = require("@aws-sdk/client-dynamodb");
-const { v4: uuidv4 } = require("uuid");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const client = new DynamoDBClient({ region: "us-west-2" });
+const MessageSchema = new Schema({
+  created: { type: Date, required: true, default: Date.now },
+  messenger: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  reciever: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  message: { type: String, maxLength: 300, trim: true, required: true },
+});
 
-const params = {
-  TableName: "Messages",
-  KeySchema: [
-    { AttributeName: "messenger", KeyType: "HASH" }, //Partition key
-    { AttributeName: "created", KeyType: "RANGE" }, //Sort key
-  ],
-  AttributeDefinitions: [
-    { AttributeName: "messenger", AttributeType: "S" },
-    { AttributeName: "created", AttributeType: "N" },
-  ],
-  ProvisionedThroughput: {
-    ReadCapacityUnits: 10,
-    WriteCapacityUnits: 10,
-  },
-};
-
-function createTable() {
-  const createTableCommand = new CreateTableCommand(params);
-
-  client
-    .send(createTableCommand)
-    .then((data) => {
-      console.log(
-        "Created table. Table description JSON:",
-        JSON.stringify(data, null, 2)
-      );
-    })
-    .catch((err) => {
-      console.error(
-        "Unable to create table. Error JSON:",
-        JSON.stringify(err, null, 2)
-      );
-    });
-}
+module.exports = mongoose.model("Message", MessageSchema);
--- server/models/User.js
+++ server/models/User.js
@@ -1,46 +1,24 @@
-const AWS = require('aws-sdk');
-const { v4: uuidv4 } = require('uuid');
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-AWS.config.update({
-  region: 'us-west-2', // replace with your AWS region
-  accessKeyId: 'your-access-key-id',
-  secretAccessKey: 'your-secret-access-key',
+const UserSchema = new Schema({
+  name: { type: String, required: true, trim: true },
+  email: { type: String, required: true, trim: true, select: false },
+  password: { type: String, trim: true, required: true, select: false },
+  age: { type: Number, min: 0, max: 110 },
+  lastUpdated: { type: Date, default: Date.now },
+  gender: { type: String, trim: true, enum: ["male", "female", "other"] },
+  bio: { type: String, trim: true, maxLength: 100 },
+  friendRequests: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  picture: {
+    type: String,
+  },
 });
+UserSchema.virtual("url").get(function () {
+  return `/profile/${this._id}`;
+});
+UserSchema.set("toObject", { virtuals: true });
+UserSchema.set("toJSON", { virtuals: true });
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
-
-const TableName = 'your-dynamodb-table-name';
-
-const createUser = async (name, email, password, age, gender, bio, picture) => {
-  const userId = uuidv4();
-  const lastUpdated = new Date();
-
-  const params = {
-    TableName,
-    Item: {
-      userId,
-      name,
-      email,
-      password,
-      age,
-      gender,
-      bio,
-      picture,
-      lastUpdated: lastUpdated.toISOString(),
-      friendRequests: [], // DynamoDB doesn't require defining an empty array
-      friends: [], // DynamoDB doesn't require defining an empty array
-    },
-  };
-
-  try {
-    await dynamoDB.put(params).promise();
-    return userId;
-  } catch (error) {
-    console.error(error);
-    throw new Error('Failed to create user');
-  }
-};
-
-module.exports = {
-  createUser,
-};
+module.exports = mongoose.model("User", UserSchema);
